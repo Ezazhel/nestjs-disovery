4e3fc5dd28a5e387a1b11c2944f8b5149ad36f9b
Steven Jeanne <Steven@jvweb.fr>
2022-03-23 23:37:52 +0100

    Add more test on controllers

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -3,9 +3,15 @@ import { CreateEventDto } from './dto/create-event.dto';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
+const createEventDto: CreateEventDto = {
+  consents: [{ enabled: true, id: 'email_notifications' }],
+  user: {
+    id: 'test',
+  },
+};
 describe('EventsController', () => {
-  let controller: EventsController;
-
+  let eventsController: EventsController;
+  let eventService: EventsService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EventsController],
@@ -24,10 +30,21 @@ describe('EventsController', () => {
       ],
     }).compile();
 
-    controller = module.get<EventsController>(EventsController);
+    eventsController = module.get<EventsController>(EventsController);
+    eventService = module.get<EventsService>(EventsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(eventsController).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create an event', () => {
+      expect(eventsController.create(createEventDto)).resolves.toEqual({
+        rowId: 1,
+        ...createEventDto,
+      });
+      expect(eventService.create).toHaveBeenCalledWith(createEventDto);
+    });
   });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,9 +3,13 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+const createUserDto: CreateUserDto = {
+  email: 'test@mail.com',
+};
 
+describe('UsersController', () => {
+  let usersController: UsersController;
+  let usersService: UsersService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -19,15 +23,65 @@ describe('UsersController', () => {
               .mockImplementation((user: CreateUserDto) =>
                 Promise.resolve({ id: 'UUID', ...user }),
               ),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                email: 'test1@mail.com',
+              },
+              {
+                email: 'test2@mail.com',
+              },
+            ]),
+            findOne: jest.fn().mockResolvedValue((id: string) =>
+              Promise.resolve({
+                mail: 'test1@mail.com',
+                id,
+              }),
+            ),
+            remove: jest.fn(),
           },
         },
       ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a user', () => {
+      expect(usersController.create(createUserDto)).resolves.toEqual({
+        id: 'UUID',
+        ...createUserDto,
+      });
+      expect(usersService.create).toHaveBeenCalledWith(createUserDto);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should find all users ', () => {
+      usersController.findAll();
+      expect(usersService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should find a user', () => {
+      expect(usersController.findOne('UUID')).resolves.toEqual({
+        mail: 'test1@mail.com',
+        id: 'UUID',
+      });
+      expect(usersService.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe('remove()', () => {
+    it('should remove the user', () => {
+      usersController.remove('UUID2');
+      expect(usersService.remove).toHaveBeenCalled();
+    });
   });
 });
